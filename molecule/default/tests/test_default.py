9c4375a4822510115f954cce6c81cbbe4d977e86
Hadrien Patte <hadrien.patte@protonmail.com>
2018-12-24 23:29:08 +0100

    Feat: install CLI utilities
    Signed-off-by: Hadrien Patte <hadrien.patte@protonmail.com>

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,28 @@
+import os
+
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('name', [
+    ('git'),
+    ('vim'),
+    ('htop'),
+    ('ncdu'),
+    ('nmap'),
+    ('sshuttle'),
+    ('p7zip-full'),
+    ('gnupg2'),
+    ('unrar'),
+    ('sl'),
+    ('cmatrix'),
+    ('cowsay'),
+    ('traceroute'),
+])
+def test_package_is_installed(host, name):
+    package = host.package(name)
+    assert package.is_installed
