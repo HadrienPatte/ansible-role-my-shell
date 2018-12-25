bdc47dd98f4db07e39f419bd5252a778968f7920
Hadrien Patte <hadrien.patte@protonmail.com>
2018-12-25 04:32:54 +0100

    Feat: install pip
    Signed-off-by: Hadrien Patte <hadrien.patte@protonmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -23,7 +23,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     ('cowsay'),
     ('traceroute'),
     ('autojump'),
+    ('python3-dev'),
 ])
-def test_package_is_installed(host, name):
+def test_apt_package_is_installed(host, name):
     package = host.package(name)
     assert package.is_installed
+
+
+@pytest.mark.parametrize('name', [
+    ('pip'),
+])
+def test_pip_package_is_installed(host, name):
+    package = host.file('/home/test-user/.local/bin/' + name)
+    assert package.exists
+    assert package.is_file
