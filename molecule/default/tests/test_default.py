f0002efe0c0e578f4b666a08d63683c926c85f6f
Hadrien Patte <hadrien.patte@protonmail.com>
2020-02-23 04:09:45 +0100

    Update test configuration to support molecule 3.0.X
    Signed-off-by: Hadrien Patte <hadrien.patte@protonmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,7 +12,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 with open('../../defaults/main.yml') as vars_yml:
     vars = yaml.load(vars_yml)
 
-with open('playbook.yml') as playbook_yml:
+with open('converge.yml') as playbook_yml:
     playbook = yaml.load(playbook_yml)
 
 vars.update(playbook[0]['vars'])
